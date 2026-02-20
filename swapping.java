e4530aa309ed1ad1f497db11755a1c82a6389287
Parth-Kale-2008 <parthkale416@gmail.com>
2026-02-20 13:26:43 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,9 @@
+public class swapping{
+    public static void main(String[] args){
+        int a = 57,b=69;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("the numbers after swapping are "+a+" and "+b);
+    }
+}
